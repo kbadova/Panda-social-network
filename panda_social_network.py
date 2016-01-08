c7eee18113213710243fd01c394de571dfd02e05
jokuf <jokuf2010@gmail.com>
2016-01-08 08:34:10 +0200

    new method - how_many_gender_in_network()

--- panda_social_network.py
+++ panda_social_network.py
@@ -1,10 +1,27 @@
 from collections import deque
+import sqlite3
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, 'panda_network.db')
+DB = sqlite3.connect(DB_PATH)
+c = DB.cursor()
+# c.execute('''CREATE TABLE IF NOT EXISTS users
+#     (ID INTEGER PRIMARY KEY     AUTOINCREMENT,
+#     name            CHAR(70)     NOT NULL,
+#     mail CHAR(20) NOT NULL),
+#     gender char(20) NOT NULL;''')
 
+# c.execute(
+#     '''
+#         CREATE TABLE IF NOT EXISTS 
+
+#     ''')
 
 class panda_social_network:
 
     def __init__(self):
         self.graph = {}
+        self.level = {}
 
     def add_panda(self, panda):
         if panda in self.graph:
@@ -35,30 +52,43 @@ class panda_social_network:
         return self.graph[panda]
 
     def connection_level(self, panda1, panda2):
-        print(self.graph)
+        print(self.level)
         return self.bfs(panda1, panda2)
 
     """
     Breadth-First-Search
     S -all you can reach in 0 moves
     """
-    def bfs(self, panda1, panda2):
-        level = {panda1: 0}  # level 0
+    def bfs(self, panda1, panda2='optional'):
+        self.level = {panda1: 0}  # level 0
         parent = {panda1: None}
-        i = 1  #start from level 1
+        i = 1  # start from level 1
         visited = [panda1]  # level i -1
 
         while visited:
-            next_graph = deque()  #level i
+            next_graph = deque()  # level i
             for node in visited:
                 for v in self.graph[node]:
-                    if v not in level:
-                        level[v] = i
+                    if v not in self.level:
+                        self.level[v] = i
                         parent[v] = node
                         next_graph.append(v)
             visited = next_graph
             i += 1
-        return level[panda2]
+        try:
+            return self.level[panda2]
+        except KeyError:
+            return False
+
+    def are_connected(self, panda1, panda2):
+        if self.bfs(panda1, panda2):
+            return True
+        return False
+
+    def how_many_gender_in_network(self, level, panda, gender):
+        self.bfs(panda)
+        key = list(self.level.keys())[list(self.level.values()).index(level)]
+        return key
 
     def save(file_name):
         pass

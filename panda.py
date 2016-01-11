5184c83d393d34a3984a306038d5064938d0e5de
jokuf <jokuf2010@gmail.com>
2016-01-11 08:34:10 +0200

    added LinkedList, Node and HashTable classes

--- panda.py
+++ panda.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 
 class Panda(object):
@@ -9,19 +10,19 @@ class Panda(object):
         self.mail = mail
         self.gender = gender
         self.__verify_mail()
-        
+
     def __str__(self):
-        return "{} {} {}".format(self.name, self.mail, self.gender)
+        return "{}, {}, {}".format(self.name, self.mail, self.gender)
 
     def __repr__(self):
-        return "{} {} {}".format(self.name, self.mail, self.gender)
+        return "{}, {}, {}".format(self.name, self.mail, self.gender)
 
     def __eq__(self, other):
         if self.__hash__() == other:
             return True
 
     def __hash__(self):
-        return hash(self.__str__())
+        return hash(self.__str__()) % ((sys.maxsize + 1) * 2)  # return always positive hash int
 
     def __verify_mail(self):
         find = re.compile('([\w_.\-]+[@]+[\w{2,10}]+[.]+[a-zA-Z]{2,5})')
